0da586cc1efc1047f3498fe7795ba448b3adff21
Chandler Calkins <chandlerjaycalkins@gmail.com>
2022-11-15 18:26:28 -0800

    added test programs for moving the servos, as well as a small angle converter lilbrary

---
+++ angle_convert.py
@@ -0,0 +1,20 @@
+min_angle = 0
+max_angle = 360
+min_pos = 0
+max_pos = 4095
+
+# convert angle to position that dynamixel servo can read
+# (angle between 0 and 360)
+def angle_to_pos(angle: float):
+    if (angle < min_angle or angle > max_angle):
+        raise Exception(f'Angle must be between 0 and {max_angle}')
+    
+    return int(angle * max_pos / max_angle)
+
+# convert dynamixel servo position to angle
+# (position between 0 and 4095)
+def pos_to_angle(pos: float):
+    if (pos < min_pos or pos > max_pos):
+        raise Exception(f'Position must be between 0 and {max_pos}')
+    
+    return pos * max_angle / max_pos
