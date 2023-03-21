7033dcaedb30e135131c304a4a85dd90895847f1
chandlerjaycalkins <chandlerjaycalkins@gmail.com>
2023-03-21 12:41:52 -0700

    Fixed servo class global values

--- src/robot_parts/servos.py
+++ src/robot_parts/servos.py
@@ -18,38 +18,38 @@ from utils.unit_convert import *
 # Used in wait for angle
 import time
 
+# These values are for X Series servos (X330 (5.0 V recommended), X430, X540, 2X430)
+# Make sure these values are correct according to the emanual for the device you're using
+# DON'T CHANGE THESE VALUES UNLESS YOU KNOW WHAT YOU'RE DOING
+
+# Address for enabling / disabling torque on a servo
+addr_torque = 64
+# Address for setting a new goal position, causing the servo to rotate
+addr_goal_pos = 116
+# Address of a servo's current position
+addr_curr_pos = 132
+# Address for setting a new velocity to control the speed of a servo
+addr_goal_vel = 104
+# Address of a servo's current velocity
+addr_curr_vel = 128
+# Bits per second that gets transmitted across the servo connection
+baudrate = 57600 # TODO: see if this value can be changed to something between 9,600 and 4,500,000 (higher is better)
+
+# Use the actual port assigned to the U2D2.
+# ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
+# Use "cat /dev/ttyUSB*" command on raspberry pi CLI to see which usb ports the servos might be connected to
+device_path = '/dev/ttyUSB0'
+# initialize PortHandler object to read and write through physical (USB) port
+port_num = PortHandler(device_path)
+
+# Dynamixel Protocol Version 2.0
+# more info:
+# https://emanual.robotis.com/docs/en/dxl/protocol2/
+protocol_version = 2.0
+# Initialize PacketHandler object to use read and write values to servos
+packet_handler = PacketHandler(protocol_version)
+
 class servo:
-	# These values are for X Series servos (X330 (5.0 V recommended), X430, X540, 2X430)
-	# Make sure these values are correct according to the emanual for the device you're using
-	# DON'T CHANGE THESE VALUES UNLESS YOU KNOW WHAT YOU'RE DOING
-
-	# Address for enabling / disabling torque on a servo
-	addr_torque = 64
-	# Address for setting a new goal position, causing the servo to rotate
-	addr_goal_pos = 116
-	# Address of a servo's current position
-	addr_curr_pos = 132
-	# Address for setting a new velocity to control the speed of a servo
-	addr_goal_vel = 104
-	# Address of a servo's current velocity
-	addr_curr_vel = 128
-	# Bits per second that gets transmitted across the servo connection
-	baudrate = 57600 # TODO: see if this value can be changed to something between 9,600 and 4,500,000 (higher is better)
-
-	# Use the actual port assigned to the U2D2.
-	# ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
-	# Use "cat /dev/ttyUSB*" command on raspberry pi CLI to see which usb ports the servos might be connected to
-	device_path = '/dev/ttyUSB0'
-	# initialize PortHandler object to read and write through physical (USB) port
-	port_handler = PortHandler(device_path)
-
-	# Dynamixel Protocol Version 2.0
-	# more info:
-	# https://emanual.robotis.com/docs/en/dxl/protocol2/
-	protocol_version = 2.0
-	# Initialize PacketHandler object to use read and write values to servos
-	packet_handler = PacketHandler(protocol_version)
-
 	########################################################################################################################
 	#
 	#
@@ -63,11 +63,11 @@ class servo:
 	# Use this at the start of your program to be able to use the servos
 	def initServos() -> bool:
 		# Open port
-		if not port_handler.openPort():
+		if not port_num.openPort():
 			return False
 
 		# Set port baudrate
-		if port_handler.setBaudRate(baudrate):
+		if port_num.setBaudRate(baudrate):
 			return True
 		else:
 			return False
@@ -85,7 +85,7 @@ class servo:
 	# Closes the port to the servos
 	# Use this when you're done with the servos / at the end of your program
 	def closeServos():
-		port_handler.closePort()
+		port_num.closePort()
 
 	########################################################################################################################
 	#
@@ -98,7 +98,7 @@ class servo:
 	# Enable torque on a servo
 	# Returns true if it successfully wrote to the servo, false if it didn't
 	def enableTorque(self) -> bool:
-		result, error = packet_handler.write1ByteTxRx(port_handler, self.id, addr_torque, 1)
+		result, error = packet_handler.write1ByteTxRx(port_num, self.id, addr_torque, 1)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -111,7 +111,7 @@ class servo:
 	# Disable torque on a servo
 	# Returns true if it successfully wrote to the servo, false if it didn't
 	def disableTorque(self) -> bool:
-		result, error = packet_handler.write1ByteTxRx(port_handler, self.id, addr_torque, 0)
+		result, error = packet_handler.write1ByteTxRx(port_num, self.id, addr_torque, 0)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -123,7 +123,7 @@ class servo:
 
 	# Returns true if the servo's torque is enabled, false if it isn't or if there was an error
 	def torqueOn(self) -> bool:
-		value, result, error = packet_handler.read1ByteTxRx(port_handler, self.id, addr_torque)
+		value, result, error = packet_handler.read1ByteTxRx(port_num, self.id, addr_torque)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -148,7 +148,7 @@ class servo:
 	# Set angle of a servo
 	# Returns true if it successfully wrote to the servo, false if it didn't
 	def setAngle(self, angle: float) -> bool:
-		result, error = packet_handler.write4ByteTxRx(port_handler, self.id, addr_goal_pos, angleToPos(angle))
+		result, error = packet_handler.write4ByteTxRx(port_num, self.id, addr_goal_pos, angleToPos(angle))
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -161,7 +161,7 @@ class servo:
 	# Set the position of a servo
 	# Returns true if it successfully wrote to the servo, false if it didn't
 	def setPos(self, pos: int) -> bool:
-		result, error = packet_handler.write4ByteTxRx(port_handler, self.id, addr_goal_pos, pos)
+		result, error = packet_handler.write4ByteTxRx(port_num, self.id, addr_goal_pos, pos)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -174,7 +174,7 @@ class servo:
 	# Reads and returns the current angle of a servo
 	# Returns false if the servo wasn't read from successfully
 	def getAngle(self):
-		current_position, result, error = packet_handler.read4ByteTxRx(port_handler, self.id, addr_curr_pos)
+		current_position, result, error = packet_handler.read4ByteTxRx(port_num, self.id, addr_curr_pos)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -187,7 +187,7 @@ class servo:
 	# Reads and returns the current position of a servo
 	# returns false if the servo wasn't read from successfully
 	def getPos(self):
-		current_position, result, error = packet_handler.read4ByteTxRx(port_handler, self.id, addr_curr_pos)
+		current_position, result, error = packet_handler.read4ByteTxRx(port_num, self.id, addr_curr_pos)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
@@ -215,14 +215,14 @@ class servo:
 	#
 	# Velocity Control
 	#
-	# Note: Veclocity values are measured on the servos in united of 0.229 revolutions / minute
+	# Note: Veclocity values are measured on the servos in united of 0.229 revolutions / minute and range from 0 to 32,767
 	#
 	#
 	########################################################################################################################
 
 	# Sets the velocity of a servo
 	def setVel(self, vel: int) -> bool:
-		result, error = packet_handler.write4ByteTxRx(port_handler, self.id, addr_goal_vel, vel)
+		result, error = packet_handler.write4ByteTxRx(port_num, self.id, addr_goal_vel, vel)
 		if result != COMM_SUCCESS:
 			print("%s" % packet_handler.getTxRxResult(result))
 			return False
