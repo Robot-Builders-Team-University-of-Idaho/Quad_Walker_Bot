372830ddcdc7b3f34b44f6681345a8345830e83d
Chandler Calkins <chandlerjaycalkins@gmail.com>
2023-03-28 20:28:31 -0700

    Added sine wave test program
    sine_test.py
    1. Added sine test program that Garret made to test mapping sine waves to servo angles to make them move smoothly.

---
+++ src/sine_test.py
@@ -0,0 +1,35 @@
+from robot_parts.legs import *
+from datetime import datetime
+import math as m
+
+servo.connect()
+
+fr = leg(1, 2, 3)
+
+#fr.a.setAngle(200)
+#fr.b.setAngle(200)
+#fr.c.setAngle(200)
+
+ti = datetime.now()
+tact = 0
+
+a = 45
+z = 0
+p = 0.00025
+
+while True:
+	t = datetime.now() - ti
+	tact = t.microseconds + (t.seconds * 1000000)
+	angle = m.degrees(m.radians(a) * m.sin(m.radians((p * tact) + z)) + m.radians(a))
+	fr.a.setAngle(angle+135)
+	fr.b.setAngle(angle+135)
+	fr.c.setAngle(angle+135)
+	print(angle+90)
+	
+	if (tact >= 4000000):
+		break
+		
+	
+	
+
+servo.close()
