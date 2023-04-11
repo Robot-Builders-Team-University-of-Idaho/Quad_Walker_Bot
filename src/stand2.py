79bfd5cf27af7d5b39483b2903e959ae894934b1
chandlerjaycalkins <chandlerjaycalkins@gmail.com>
2023-04-11 14:46:27 -0700

    Updated stand2.py test program to work with getSine updates
    stand2.py
    1. Updated test program to work with the added forward parameter to the getSine functions.

--- src/stand2.py
+++ src/stand2.py
@@ -52,12 +52,12 @@ while time_passed < 4_000_000:
 	time_passed = datetime.now() - start_time
 	time_passed = (time_passed.seconds * 1_000_000) + time_passed.microseconds
 	
-	fl_angle_a = getSineAngle(time_passed, 135, 225, 180, 25) #(lower angle, upper angle, start angle, %speed)
-	fr_angle_a = getSineAngle(time_passed, 135, 225, 135, 25)
-	bl_angle_a = getSineAngle(time_passed, 135, 225, 225, 25)
-	br_angle_a = getSineAngle(time_passed, 135, 225, 180, 25)
+	fl_angle_a = getSineAngle(time_passed, 135, 225, 180, True, 25) #(lower angle, upper angle, start angle, %speed)
+	fr_angle_a = getSineAngle(time_passed, 135, 225, 135, True, 25)
+	bl_angle_a = getSineAngle(time_passed, 135, 225, 225, True, 25)
+	br_angle_a = getSineAngle(time_passed, 135, 225, 180, True, 25)
 	
-	fl_angle_b = getSineAngle(time_passed, 150, 190, 190, 25)
+	fl_angle_b = getSineAngle(time_passed, 150, 190, 190, True, 25)
 	mid = (190 - 150) / 2
 	
 	legs[0].a.setAngle(fl_angle_a)
