328a1ce283397f519d1821de1b2fa531e050a150
chandlerjaycalkins <chandlerjaycalkins@gmail.com>
2023-04-16 01:56:49 -0700

    Added comments to turn and walk programs
    turn.py
    1. Removed redundant setAngles on c joints when robot is standing up.
    2. Added comments for everything in the program.
    
    walk.py
    1. Removed redundant setAngles on c joints when robot is standing up.
    2. Added comments for everything in the program.

--- src/turn.py
+++ src/turn.py
@@ -2,12 +2,14 @@ from robot_parts.legs import *
 from datetime import datetime
 import time
 
+# define a and b joint high, low, and mid angles
 a_low = 150
 a_high = 210
 b_low = 110
 b_high = 190
 b_mid = (b_high + b_low) / 2
 
+# establish connection with servos
 servo.connect()
 
 # initialize legs and servos
@@ -17,6 +19,7 @@ for i in range(leg_count):
 	n = i * 2 + i + 1
 	legs.append(leg(n, n+1, n+2))
 
+# set all legs out straight
 for i in range(leg_count):
 	legs[i].a.setAngle(180)
 	legs[i].b.setAngle(180)
@@ -24,32 +27,32 @@ for i in range(leg_count):
 
 time.sleep(0.75)
 
+# disable torque on b joint of each leg
 for i in range(leg_count):
 	legs[i].b.torqueOff()
 
+# curl c joints in towards body to make it easier to stand up
 for i in range(leg_count):
 	legs[i].c.setAngle(90)
 
 time.sleep(0.75)
 
+# re-enable torque on b joints to stand up
 for i in range(leg_count):
 	legs[i].b.torqueOn()
 
+# stand up making sure a joints for each leg are in correct start position
 legs[0].a.setAngle(a_low)
 legs[0].b.setAngle(b_mid)
-legs[0].c.setAngle(110)
 
 legs[1].a.setAngle(a_high)
 legs[1].b.setAngle(b_mid)
-legs[1].c.setAngle(110)
 
 legs[2].a.setAngle(a_high)
 legs[2].b.setAngle(b_mid)
-legs[2].c.setAngle(110)
 
 legs[3].a.setAngle(a_low)
 legs[3].b.setAngle(b_mid)
-legs[3].c.setAngle(110)
 
 time.sleep(2)
 
@@ -57,6 +60,7 @@ start = datetime.now()
 now = 0
 speed = 25
 
+# turn for 8 seconds
 while now < 8_000_000:
 	now = datetime.now() - start
 	now = (now.seconds * 1_000_000) + now.microseconds
@@ -65,9 +69,11 @@ while now < 8_000_000:
 	legs[2].walk(False, now, speed=speed)
 	legs[3].walk(True, now, speed=speed)
 
+# disable torque on all servos
 for i in range(leg_count):
 	legs[i].a.torqueOff()
 	legs[i].b.torqueOff()
 	legs[i].c.torqueOff()
 
+# close connection to servos
 servo.close()
